f1d3be6272d2c857da8ed4abb122bcd015a9bfee
Kari Gilbertson <gilberk1@tcnj.edu>
2016-05-01 22:27:03 -0400

    functionality finished

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 var url = "https://fake-people.benspoon.com/2d8c35ce/people";
-var id;
 
 $(document).ready(function() {
   $.ajax({
@@ -11,8 +10,17 @@ $(document).ready(function() {
       $('.person-id').on("click", function(){
           personId = $(this).data('id');
           $('#instructions').css({display: "none"});
+          $('#edit').css({display: "block"});
+          $('#delete').css({display: "block"});
           $('#information').css({display: "block"});
           $('#new-person').css({display: "none"});
+          $('#post-person').css({display: "none"});
+          $('#error').css({display: "none"});
+          $('#edit-person').css({display: "none"});
+          $('#patch-person').css({display: "none"});
+          $('#remove-person').css({display: "none"});
+          $('#undo').css({display: "none"});
+          $('#delete-person').css({display: "none"});
 
           $.ajax({
             url: url + "/" + personId,
@@ -30,6 +38,102 @@ $(document).ready(function() {
               $('#email').text("Email: " + res.email);
               $('#phone').text("Phone: " + res.phone);
               $('#address').html("Address: <br/>" + address + "<br/>" + city + ", " + state + ", " + zip);
+
+              $('#edit').on("click", function() {
+                $('#instructions').css({display: "none"});
+                $('#edit').css({display: "none"});
+                $('#delete').css({display: "none"});
+                $('#information').css({display: "none"});
+                $('#new-person').css({display: "none"});
+                $('#post-person').css({display: "none"});
+                $('#error').css({display: "none"});
+                $('#edit-person').css({display: "block"});
+                $('#patch-person').css({display: "block"});
+                $('#remove-person').css({display: "none"});
+                $('#undo').css({display: "none"});
+                $('#delete-person').css({display: "none"});
+
+                $('input[name=edit-picture]').filter("[value='" + res.picture + "']").attr('checked', true);
+                $('#edit-name').val(res.name);
+                $('#edit-age').val(res.age);
+                $('#edit-eyeColor').val(res.eyeColor);
+                $('#edit-gender').val(res.gender);
+                $('#edit-email').val(res.email);
+                $('#edit-phone').val(res.phone);
+                $('#edit-address').val(address);
+                $('#edit-city').val(city);
+                $('#edit-state').val(state);
+                $('#edit-zipCode').val(zip);
+
+                $('#patch-person').on("click", function() {
+                  var person = {};
+
+                  person.picture = $('input[name=edit-picture]:checked').val();
+                  person.name = $('#edit-name').val();
+                  person.age = $('#edit-age').val();
+                  person.eyeColor = $('#edit-eyeColor').val();
+                  person.gender = $('#edit-gender').val();
+                  person.email = $('#edit-email').val();
+                  person.phone = $('#edit-phone').val();
+                  person.address = $('#edit-address').val() + ", "
+                                    + $('#edit-city').val() + ", "
+                                    + $('#edit-state').val() + ", "
+                                    + $('#edit-zipCode').val();
+                  $.ajax({
+                    url: url + "/" + personId,
+                    success: function(res) {
+                      $.ajax({
+                        method: "PATCH",
+                        url: url + "/" + res.id,
+                        data: person,
+                        success: function (res) {
+                          location.reload();
+                        }
+                      });
+                    }
+                  });
+                });
+              });
+              $('#delete').on("click", function() {
+                $('#instructions').css({display: "none"});
+                $('#edit').css({display: "none"});
+                $('#delete').css({display: "none"});
+                $('#information').css({display: "none"});
+                $('#new-person').css({display: "none"});
+                $('#post-person').css({display: "none"});
+                $('#error').css({display: "none"});
+                $('#edit-person').css({display: "none"});
+                $('#patch-person').css({display: "none"});
+                $('#remove-person').css({display: "block"});
+                $('#undo').css({display: "block"});
+                $('#delete-person').css({display: "block"});
+
+                $('#undo').on("click", function() {
+                  $('#instructions').css({display: "none"});
+                  $('#edit').css({display: "block"});
+                  $('#delete').css({display: "block"});
+                  $('#information').css({display: "block"});
+                  $('#new-person').css({display: "none"});
+                  $('#post-person').css({display: "none"});
+                  $('#error').css({display: "none"});
+                  $('#edit-person').css({display: "none"});
+                  $('#patch-person').css({display: "none"});
+                  $('#remove-person').css({display: "none"});
+                  $('#undo').css({display: "none"});
+                  $('#delete-person').css({display: "none"});
+                });
+
+                $('#delete-person').on("click", function() {
+                  $.ajax({
+                    method: "DELETE",
+                    url: url + "/" + res.id,
+                    data: res,
+                    success: function (res) {
+                      location.reload();
+                    }
+                  });
+                });
+              });
             }
           });
   		});
@@ -37,121 +141,52 @@ $(document).ready(function() {
   });
   $('#create-new').on("click", function() {
     $('#instructions').css({display: "none"});
+    $('#edit').css({display: "none"});
+    $('#delete').css({display: "none"});
     $('#information').css({display: "none"});
     $('#new-person').css({display: "block"});
     $('#post-person').css({display: "block"});
+    $('#error').css({display: "none"});
+    $('#edit-person').css({display: "none"});
+    $('#patch-person').css({display: "none"});
   });
   $('#post-person').on("click", function() {
-      var person = {};
+    var person = {};
+    
+    person.picture = $('input[name=picture]:checked').val();
+    person.name = $('#new-name').val();
+    person.age = $('#new-age').val();
+    person.eyeColor = $('#new-eyeColor').val();
+    person.gender = $('#new-gender').val();
+    person.email = $('#new-email').val();
+    person.phone = $('#new-phone').val();
+    person.address = $('#new-address').val() + ", "
+                      + $('#new-city').val() + ", "
+                      + $('#new-state').val() + ", "
+                      + $('#new-zipCode').val();
 
-      person.name = $('#new-name').val();
-      person.age = $('#new-age').val();
-      person.eyeColor = $('#new-eyeColor').val();
-      person.gender = $('#new-gender').val();
-      person.email = $('#new-email').val();
-      person.phone = $('#new-phone').val();
-      person.address = $('#new-address').val() + ", "
-                        + $('#new-city').val() + ", "
-                        + $('#new-state').val() + ", "
-                        + $('#new-zipCode').val();
+    if(person.name != "" &&
+        person.age != "" &&
+        person.eyeColor != "" &&
+        person.gender != "" &&
+        person.email != "" &&
+        person.phone != "" &&
+        $('#new-address').val() != "" &&
+        $('#new-city').val() != "" &&
+        $('#new-state').val() != "" &&
+        $('#new-zipCode').val() != "") {
 
-      if(person.name != "" &&
-          person.age != "" &&
-          person.eyeColor != "" &&
-          person.gender != "" &&
-          person.email != "" &&
-          person.phone != "" &&
-          person.address != "") {
-
-          $.ajax({
-          method: "POST",
-          url: url,
-          data: person,
-          success: function (res) {
-            location.reload();
-          }
-        });
-      }
-      else {
-        $('#error').css({display: "block"});
-      }
-  });
-  $('#patch-person').on("click", function() {
-      var person = {};
-
-      person.name = $('#edit-name').val();
-      person.age = $('#edit-age').val();
-      person.eyeColor = $('#edit-eyeColor').val();
-      person.gender = $('#edit-gender').val();
-      person.email = $('#edit-email').val();
-      person.phone = $('#edit-phone').val();
-      person.address = $('#edit-address').val() + ", "
-                        + $('#edit-city').val() + ", "
-                        + $('#edit-state').val() + ", "
-                        + $('#edit-zipCode').val();
-
-      document.getElementById('edit-name').value = '';
-      document.getElementById('edit-age').value = '';
-      document.getElementById('edit-eyeColor').value = '';
-      document.getElementById('edit-gender').value = '';
-      document.getElementById('edit-email').value = '';
-      document.getElementById('edit-phone').value = '';
-      document.getElementById('edit-address').value = '';
-      document.getElementById('edit-city').value = '';
-      document.getElementById('edit-state').value = '';
-      document.getElementById('edit-zipCode').value = '';
-
-      $.ajax({
+        $.ajax({
+        method: "POST",
         url: url,
-        success: function(res) {
-          var personId;
-          res.forEach(function(possible){
-            if(possible.name == person.name) {
-              personId = possible.id
-            }
-          });
-          $.ajax({
-            url: url + "/" + personId,
-            success: function(res) {
-              $.ajax({
-                method: "PATCH",
-                url: url + "/" + res.id,
-                data: person,
-                success: function (res) {
-                }
-              });
-            }
-          });
+        data: person,
+        success: function (res) {
+          location.reload();
         }
       });
-  });
-  $('#delete-person').on("click", function() {
-      var person = {};
-
-      person.name = $('#delete-name').val();
-
-      $.ajax({
-        url: url,
-        success: function(res) {
-          var personId;
-          res.forEach(function(possible){
-            if(possible.name == person.name) {
-              personId = possible.id
-            }
-          });
-          $.ajax({
-            url: url + "/" + personId,
-            success: function(res) {
-              $.ajax({
-                method: "DELETE",
-                url: url + "/" + res.id,
-                data: person,
-                success: function (res) {
-                }
-              });
-            }
-          });
-        }
-      })
+    }
+    else {
+      $('#error').css({display: "block"});
+    }
   });
 });
